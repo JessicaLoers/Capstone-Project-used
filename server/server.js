64fcb9bdacaf766d61d4bd3a16c9b1b38ecad1e9
Jessica Lörs <54777853+JessicaLoers@users.noreply.github.com>
2022-01-09 19:42:25 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,6 @@ server.use(cors())
 server.use(express.json()) // JSON Body Parser
 
 // const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`
-const connectionString = `mongodb+srv://used_admin:8La4D6VcX1aoCKEX@cluster0.3peu2.mongodb.net/usedDatabase?retryWrites=true&w=majority`
 mongoose.connect(connectionString)
 
 
