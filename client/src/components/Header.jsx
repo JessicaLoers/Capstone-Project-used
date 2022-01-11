89630e4edb77f9c0300287e039276426cb73cae7
Jessica Loers <jessica.loers@gmail.com>
2022-01-11 21:16:11 +0100

    raw styling sreachbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,26 +1,19 @@
-import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 export default function Header({ isStatic }) {
   return (
     <StyledHeader isStatic={isStatic}>
-   <h3>Logo</h3>
+      <h3>Logo</h3>
     </StyledHeader>
-  );
+  )
 }
 
 const StyledHeader = styled.header`
-  background: #5b81ee;
+  background: var(--secondarycolor);
   display: flex;
   flex-direction: row;
-  justify-content: left;
-  position: ${(props) => (props.isStatic ? 'static' : 'fixed')};
+  justify-content: center;
   top: 0;
   right: 0;
   left: 0;
-  z-index: 100;
-`;
-
-Header.propTypes = {
-  isStatic: PropTypes.bool,
-};
+`
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -3,27 +3,108 @@ import ArtistCard from './ArtistCard'
 import tracksdata from '../lib/tracksdata'
 import { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import icon_close from '../assets/icons/icon_close.svg'
+import icon_search from '../assets/icons/icon_search.svg'
 
 export default function SearchBar({ artists }) {
   const [trackdata, setTrackdata] = useState('')
 
   return (
-    <SearchBarWrapperStyled>
-      {artists?.map((artist) => (
-        <div>
-          <ArtistCard
-            
-            artistName={artist.artistName}
-            artist_image={artist.artist_image}
+    <WrapperStyled>
+      <SearchBarWrapperStyled>
+        <ToggleBtnPair>
+          <button>track</button>
+          <button>artist</button>
+        </ToggleBtnPair>
+
+        <SearchInput>
+          <input
+            className='searchinput'
+            type='text'
+            placeholder='search ... '
           />
-        </div >
-      ))}
-    </SearchBarWrapperStyled>
+          <img className='closeIcon' src={icon_close} alt='close icon' />
+          <img className='searchIcon' src={icon_search} alt='search icon' />
+        </SearchInput>
+      </SearchBarWrapperStyled>
+
+      <Results>
+        {artists?.map((artist) => (
+          <div>
+            <ArtistCard
+              artistName={artist.artistName}
+              artist_image={artist.artist_image}
+            />
+          </div>
+        ))}
+      </Results>
+    </WrapperStyled>
   )
 }
 
+const WrapperStyled = styled.div`
+  margin-bottom: 5rem;
+`
 const SearchBarWrapperStyled = styled.div`
+  padding-top: 1.6rem;
+  padding-bottom: 1rem;
+  background-color: var(--secondarycolor);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: sticky;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 30;
+`
+const ToggleBtnPair = styled.div`
+  display: flex;
+  align-self: center;
+  margin-bottom: 1rem;
+`
+const Results = styled.div`
   margin-top: 3rem;
+`
+const SearchInput = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+
+  .searchinput {
+    background-color: var(--lightgrey);
+    border: 0;
+    border-radius: 2px;
+    font-size: 0.9rem;
+    padding: 1rem;
+    height: 1.8rem;
+    width: 60vw;
+  }
+  .searchinput:focus {
+    outline: none;
+  }
+  .searchIcon {
+    height: 24px;
+    width: 24px;
+    background-color: var(--lightgrey);
+    display: grid;
+    place-items: center;
+    right: 6rem;
+    bottom: 3px;
+    position: absolute;
+    z-index: 10;
+  }
+  .closeIcon {
+    height: 24px;
+    width: 24px;
+    background-color: var(--lightgrey);
+    display: grid;
+    place-items: center;
+    right: 6rem;
+    bottom: 3px;
+    position: absolute;
+    z-index: 20;
+  }
 `
 
 {
